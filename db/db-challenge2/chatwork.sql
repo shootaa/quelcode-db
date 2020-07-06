4dc3ba9aa22855d19b9c0aebdc1004724253a9a7
shootaa <60875264+shootaa@users.noreply.github.com>
2020-07-06 18:57:36 +0900

    指摘対応

--- db/db-challenge2/chatwork.sql
+++ db/db-challenge2/chatwork.sql
@@ -2,12 +2,12 @@ CREATE TABLE `chats` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `message` varchar(1000) NOT NULL,
       `filename` varchar(100),
-      `chatroom_id` int(11) NOT NULL REFERENCES chatrooms(ID),
-      `is_deleted` tinyint DEFAULT 0 NOT NULL,
+      `chatroom_id` int(11) NOT NULL REFERENCES chatrooms(id),
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
       `created_at` datetime NOT NULL,
-      `created_by` int(11) NOT NULL,
+      `created_by` int(11) NOT NULL REFERENCES users(id),
       `updated_at` datetime NOT NULL,
-      `updated_by` int (11) NOT NULL,
+      `updated_by` int (11) NOT NULL REFERENCES users(id),
 
       PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -16,20 +16,20 @@ CREATE TABLE `chatrooms` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `chatroom_name` varchar(100) NOT NULL,
       `description` varchar(1000),
-      `file_permission` tinyint DEFAULT 0 NOT NULL,
-      `is_directed` tinyint DEFAULT 0 NOT NULL,
-      `is_deleted` tinyint DEFAULT 0 NOT NULL,
+      `is_file_sendable` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_direct_chat` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
       `created_at` datetime NOT NULL,
-      `created_by` int(11) NOT NULL,
+      `created_by` int(11) NOT NULL REFERENCES users(id),
       `updated_at` datetime NOT NULL,
-      `updated_by` int(11) NOT NULL,
+      `updated_by` int(11) NOT NULL REFERENCES users(id),
 
       PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `chatrooms_users` (
-      `user_id` int(11) NOT NULL REFERENCES users(ID),
-      `chatroom_id` int(11) NOT NULL REFERENCES chatrooms(ID),
+      `user_id` int(11) NOT NULL REFERENCES users(id),
+      `chatroom_id` int(11) NOT NULL REFERENCES chatrooms(id),
       `join_at` datetime NOT NULL,
       PRIMARY KEY (`user_id`,`chatroom_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -40,11 +40,11 @@ CREATE TABLE `users` (
       `email` varchar(100) NOT NULL,
       `password` varchar(100)  NOT NULL,
       `profile` varchar(100),
-      `cellphone_number` int(13),
-      `office_telephonenumber` int(13),
-      `is_deleted` tinyint DEFAULT 0 NOT NULL,
-      `created_at` tinyint NOT NULL,
-      `updated_at` tinyint NOT NULL,
+      `cellphone_number` varchar(13),
+      `office_telephonenumber` varchar(13),
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
+      `created_at` datetime NOT NULL,
+      `updated_at` datetime NOT NULL,
 
       PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -53,14 +53,14 @@ CREATE TABLE `tasks` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `task_name` varchar(1000) NOT NULL,
       `deadline` datetime,
-      `is_done` tinyint DEFAULT 0 NOT NULL,
-      `is_deleted` tinyint DEFAULT 0 NOT NULL,
-      `owner_id` int(11)  NOT NULL REFERENCES users(ID),
-      `chatroom_id` int(11)  NOT NULL REFERENCES chatrooms(ID),
+      `is_done` tinyint(1) DEFAULT 0 NOT NULL,
+      `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
+      `task_manager_id` int(11)  NOT NULL REFERENCES users(id),
+      `chatroom_id` int(11)  NOT NULL REFERENCES chatrooms(id),
       `created_at` datetime NOT NULL,
-      `created_by` int(11) NOT NULL,
+      `created_by` int(11) NOT NULL REFERENCES users(id),
       `updated_at` datetime NOT NULL,
-      `updated_by` int(11) NOT NULL,
+      `updated_by` int(11) NOT NULL REFERENCES users(id),
 
       PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
