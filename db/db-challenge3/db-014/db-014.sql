893d31d6fa60bc6c73650bc54205e2e8016539a2
shootaa <60875264+shootaa@users.noreply.github.com>
2020-07-13 15:56:06 +0900

    db011-014 やったこと ・011,012,014のSQLの修正。 ・キャプチャの追加

--- db/db-challenge3/db-014/db-014.sql
+++ db/db-challenge3/db-014/db-014.sql
@@ -1,14 +1,14 @@
 begin;
-(
-    select
-        user_id,
-        chatroom_id
-    from
-        users_chatrooms uc
-        left join users u on uc.user_id = u.id
-    where
-        u.is_deleted = 1
-) as a
 delete from
-    a 
+    users_chatrooms
+where
+    user_id in(
+        select
+            id
+        from
+             users 
+        where
+            is_deleted = 1
+    ) 
 commit;
+
